842b9c94fdb806143a6eab91657f14a99c6de9c8
wukong220 <wusong220@outlook.com>
2019-05-27 21:09:32 +0800

    Update MSD.cpp

--- MSD.cpp
+++ MSD.cpp
@@ -21,10 +21,10 @@ const int Num_chains = 1;								//Number of the chains
 const int Num_beeds = N_chain * Num_chains; 			//Number of beeds
 
 const int dimension = 2;
-const int Num_file = 1;
+const int Num_file = 20;
 std::vector<int> closefiles{};				//closefiles
 std::string finname;// = "003";				//empty or single input file
-std::string foutname = "MSD.000_0.4_3.0_1.0_3.0.txt";		
+std::string foutname = "MSD.000_0.4_2.5_1.0_3.0.txt";		
 std::string outname = "MSD.000.log";
 
 const double md_dt = 0.001;
