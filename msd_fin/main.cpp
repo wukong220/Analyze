9434e3be5962a7e3ba8b062320ee4f89e47543d9
wukong220 <wusong220@outlook.com>
2019-06-05 19:54:34 +0800

    for input upgrade

--- msd_fin/main.cpp
+++ msd_fin/main.cpp
@@ -7,43 +7,60 @@ const int dimension = 2;
 int Num_file = 20;
 
 const double mass = 1.0;
-const int N_chain = 30;									//Polarization of single chain
-const int Num_chains = 1;								//Number of the chains
-const int Num_beeds = N_chain * Num_chains; 			//Number of beeds
+int N_chain = 30;									//Polarization of single chain
+int Num_chains = 1;								//Number of the chains
+int Num_beeds = N_chain * Num_chains; 			//Number of beeds
 //vector<string> type{"1", "2"};								//atom types to read
 
 vector<int> closefiles{};				//closefiles
 string finname;// = "001";				//empty or single input file
-string foutname = "MSD.000.txt";		
-string logname = "MSD.000.log";
+string foutname = "000";		
+string logname = "000";
 
 const double md_dt = 0.001;
-const int Num_frame = 1000;
-const int dNM = 500;
-const int Max_frame = Num_frame - dNM;
+int Num_frame = 10000;
+int dNM = 2000;
+int Max_frame = Num_frame - dNM;
 const int framestep = 5000;		
+
+void input(int &x);
+void input(string &str);
+string time (clock_t start);
 
 int main() 
 {
 		
 	string str;
 	stringstream ss;
-	char ch;
 	clock_t start = clock();		//start time
 	
-	cout << "\"Number of files: (20 for default)\"\n";
-	cin >> Num_file;
+	cout << "dimension = 2;\nmass = 1;\nmd_dt = 0.001;\nframestep = 5000;\n\n"; //default information
 	
-	cout << "\"Input file (single letter for all):\" \n";
-	cin >> str;
-	if (str.length() != 1 && !(isalpha(str[0])))
-		finname = str;
-	cout << "\"Output txt file:\" \n";
-	cin >> foutname;
-	foutname += ".txt";
-	cout << "\"Log file:\" \n";
-	cin >> logname;
-	logname += ".log";
+	cout << "\"Number of chains( \'1\' for default): \"\n";
+	input(Num_chains);
+	cout << "\"Number of atoms( \'30\' for single chain default): \"\n";
+	input(N_chain); 
+	Num_beeds = N_chain * Num_chains;
+	
+	cout << "\n\"Number of files(\'20\' for default): \"\n";
+	input(Num_file);
+	if (Num_file == 1)
+	{
+		cout << "\"Input file name(without \'u.lammpstrj\'):\" \n";
+		cin >> finname;
+	}
+	cout << "\"Number of frames( \'10000\' for default): \"\n";
+	input(Num_frame);
+	cout << "\"Frames to delete( \'2000\' for default): \"\n";
+	input(dNM);
+	Max_frame = Num_frame - dNM;
+	
+	cout << "\n\"Output txt file name( \'000.MSD.txt\' for default): \" \n";
+	input(foutname);
+	foutname += ".MSD.txt";
+	cout << "\"Log file name( \'000.MSD.log\' for default): \" \n";
+	input(logname);
+	logname += ".MSD.log";
 	
 	//atom[iframe] [id] [id,type,xu,yu,zu...]
 	vec_doub3 vecAtom(Num_frame, vector<vector<double> >(Num_beeds, vector<double>(Num_info,0))); 
@@ -74,6 +91,40 @@ int main()
 	output << "\"Writing\": " << foutname << endl << "\"Outputing\": " << logname << endl;	
 	//time
 
+	str = time(start);
+	output << str << endl;
+	output.close();
+	return 0;
+}
+
+inline void input(int &x)
+{
+	stringstream ss;
+	string str;
+	getline(cin, str);
+	if(str != "\n")
+	{
+		ss << str;
+		ss >> x;
+	}
+}
+
+inline void input(string &x)
+{
+	stringstream ss;
+	string str;
+	getline(cin, str);
+	if(str != "\n")
+	{
+		ss << str;
+		ss >> x;
+	}
+}
+
+string time (clock_t start)
+{
+	stringstream ss;
+	string str;
 	clock_t stop = clock();		//#include <ctime>
 	double Time = (double)(stop - start)/CLOCKS_PER_SEC;
 	vector<int> inter(4,Time);
@@ -94,7 +145,5 @@ int main()
 		ss.clear();
 	}
 	cout << st << endl;
-	output << st << endl;
-	output.close();
-	return 0;
+	return st;
 }
