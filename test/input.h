1c3fcbdb59e025e765d4288d74988f0c16a4e1af
wukong220 <wusong220@outlook.com>
2019-06-05 11:37:56 +0800

    arrange

--- test/input.h
+++
@@ -1,9 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-#include "paras.h"
-
-vector<vector<vector<double> > > input(string n);
-//vector<vector<vector<double> > > center(vector<vector<vector<double> > > &a);
-
-#endif
--- test/paras.cpp
+++
@@ -1,7 +0,0 @@
-#include "paras.h"
-
-void output()
-{
-	cout << dNM;
-	cout << files[0] << " " << files[1] << " " << files[2] << endl;
-}
