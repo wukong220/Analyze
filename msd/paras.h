3eaef6b583e2b22b3e4906b6069940cafcd0d82c
wukong220 <wusong220@outlook.com>
2019-06-06 21:10:53 +0800

    for test

--- msd/paras.h
+++ msd/paras.h
@@ -22,8 +22,12 @@ extern int Num_frame;
 extern int dNM;
 //extern const int Max_frame;
 extern const int framestep;
-extern ofstream output;
 
+//input line by line
+bool input(int &x);
+bool input(string &str);
+//time
+string time (clock_t start);
 vec_doub3 operator+=(vec_doub3 &vec0, const vec_doub3 vec1);
 //output for check
 ostream & operator<<(ostream & os, vector<int> &vec);
@@ -34,7 +38,8 @@ ostream & operator<<(ostream & os, vector<vector<double> > &vec);
 ostream & operator<<(ostream & os, vector<vector<string> > &vec);
 //just for fun
 ostream & operator<<(ostream & os, vector<vector<vector<double> > > &vec);	
-
+//show information
+vector<string> show(string &logname, string &foutname, int &Num_chains, int &N_chain, int &Num_beeds, int &Max_frame);
 //for test
 
 #endif
