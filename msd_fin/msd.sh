e74b2e536abc78d062f564104e24d1bbe48a32cf
wukong220 <wusong220@outlook.com>
2019-06-07 17:18:40 +0800

    shell

--- msd_fin/msd.sh
+++ msd_fin/msd.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-
 # for t in {0..9}
 
-for t in 001 002 003 010 015 030  
-do
-  sed "10,19s/001/$t/g" main.cpp  > $t.msd.cpp
-  g++ -std=c++0x paras.cpp files.cpp $t.msd.cpp -o $t.msd      #for NJU
-#  g++ -std=c++11 *.cpp -o $t.msd        #for Mac
-  nohup ./$t.msd > $t.output &
-done 
+file="main.cpp"
+#for t in 001 002 003 010 015 030  
+#do
+# sed -e "10s/1/$t/g" -e "17,19s/001/0$t/g" $file  > 0$t.msd.cpp
+#  g++ -std=c++0x paras.cpp files.cpp 0$t.msd.cpp -o 0$t.msd      #for NJU
+#  nohup ./0$t.msd > 0$t.output &
+#done 
+
+name="0.0_5.0"
+
+g++ -std=c++0x paras.cpp files.cpp $file -o $name.msd
+nohup ./$name.msd > $name.output &
+#./$name.msd
